f57b6efd64ce6b5133b42dd843de9ac967612e58
jiaoshijie <jiao1751959040@gmail.com>
2019-09-06 17:11:45 +0800

    add config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ export EDITOR=/usr/bin/nvim
 ########################
 #自己定义的设置
 alias SetProxy="export http_proxy=http://127.0.0.1:8118 export https_proxy=http://127.0.0.1:8118"
-alias UnSetProxy="unset http_proxy https_proxy"
+# alias UnSetProxy="unset http_proxy https_proxy"
 alias lsnvidia="lspci | egrep 'VGA|3D'"
 alias nvidiainfo="nvidia-smi"
 # alias nvidiaup="sudo tee /proc/acpi/bbswitch <<< 'ON'"
@@ -11,8 +11,8 @@ alias p="ping www.baidu.com"
 alias c=clear
 alias x="xmodmap ~/.Xmodmap"
 alias tbvimrc="cp ~/.vimrc ~/'Nutstore Files'/Nutstore/picture/bukeshiyong/vimrc"
-alias wgu="sudo wg-quick up wg0"
-alias wgd="sudo wg-quick down wg0"
+# alias wgu="sudo wg-quick up wg0"
+# alias wgd="sudo wg-quick down wg0"
 alias hexedit="hexedit --color"
 alias ntmux="tmux new -s"
 alias atmux="tmux a -t"
