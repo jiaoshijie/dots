4d293bed296c0723a2a2f5c77e010413a5315134
jiaoshijie <jiao1751959040@gmail.com>
2020-03-28 18:23:43 +0800

    modified some configs

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,9 +3,10 @@
 /usr/bin/bash ./dwm-status.sh &
 picom &
 mate-power-manager &
-feh --bg-fill /home/red/Pictures/01_月光沙丘/exodus_v02_5120x2880.png &
+xwallpaper --zoom /home/red/Pictures/01_月光沙丘/exodus_v02_5120x2880.png &
 fcitx &
 utools &
 nm-applet &
+volumeicon &
 flameshot &
 nutstore &
