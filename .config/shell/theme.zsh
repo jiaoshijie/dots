dae60eb661d309edefc8e6b3d8085d53f87d3231
jiaoshijie <jiao1751959040@gmail.com>
2021-11-17 19:49:18 +0800

    What: modified shell/theme git `check` icons font

--- .config/shell/theme.zsh
+++ .config/shell/theme.zsh
@@ -6,7 +6,7 @@ setopt prompt_subst
 ZSH_THEME_GIT_PROMPT_PREFIX="(:"
 ZSH_THEME_GIT_PROMPT_SUFFIX=")"
 ZSH_THEME_GIT_PROMPT_DIRTY=" ✗"
-ZSH_THEME_GIT_PROMPT_CLEAN=" ✔"
+ZSH_THEME_GIT_PROMPT_CLEAN=" "
 
 local git_info='$(git_prompt_info)'
 
