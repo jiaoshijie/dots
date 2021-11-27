a6e58efeedc579983a0c2326a7b6f3b2af4bb867
jiaoshijie <jiao1751959040@gmail.com>
2021-11-27 22:04:43 +0800

    What: changed trayer position

--- .config/my_script/tray_toggle.sh
+++ .config/my_script/tray_toggle.sh
@@ -3,7 +3,7 @@
 # sudo pacman -S trayer
 result=$(ps ax|grep -v grep|grep trayer)
 if [ "$result" == "" ]; then
-  eval "trayer --edge top --align center --transparent true --tint 0x00000000 --iconspacing 7 --height 25 &"
+  eval "trayer --edge top --padding 10 --align center --distance 540 --transparent true --tint 0x00000000 --iconspacing 7 --heighttype request --widthtype request &"
 else
   eval "killall trayer"
 fi
