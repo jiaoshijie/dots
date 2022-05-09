15df44bff0ffb3d7b40c9cdffb7de9336b7f2272
jiaoshijie <jiao1751959040@gmail.com>
2022-05-09 19:26:38 +0800

    What: just update

--- my_script/tray_toggle.sh
+++ my_script/tray_toggle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # sudo pacman -S trayer
-result=$(ps ax|grep -v grep|grep trayer)
+result=$(ps ax | grep "[t]rayer")
 if [ "$result" == "" ]; then
   eval "trayer --edge top --padding 10 --align center --distance 540 --transparent true --tint 0x00000000 --iconspacing 7 --heighttype request --widthtype request &"
 else
